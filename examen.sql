9063c68690908a747061d57b4007430d85edaf63
Maritoz <maritox47@gmail.com>
2024-12-03 21:47:30 +0100

    Base libros

--- examen.sql
+++ examen.sql
@@ -6,3 +6,12 @@ CREATE TABLE IF NOT EXISTS `suscripcion` (
   `tipo_pago` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   CONSTRAINT fk_usuario_id FOREIGN KEY(`id_usuario`) REFERENCES `usuario`(`id_usuario`)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `Libros` (
+  `id_libro` INT AUTO_INCREMENT NOT NULL,
+  `titulo` VARCHAR(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `autor` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `año_publicacion` YEAR NOT NULL,
+  `precio` DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (`id_libro`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
